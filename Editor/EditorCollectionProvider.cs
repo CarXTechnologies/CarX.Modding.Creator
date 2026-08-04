44e60a39397deb8f984f411539f2c78c0529854c
Evgenie Myasnikov <e.myasnikov@carx-tech.com>
2026-08-04 14:02:19 +0300

    refactor

--- Editor/EditorCollectionProvider.cs
+++ Editor/EditorCollectionProvider.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 namespace Plugins.CarX.Modding.Creator.Editor
 {
-    public class EditorCollectionProvider : ProviderCollection
-    {
-        private static readonly IModFileProvider DefaultFileProvider = new DefaultFileProvider(Path.Combine(Application.dataPath, "Mods"));
+	public class EditorCollectionProvider : ProviderCollection
+	{
+		private static readonly IModFileProvider DefaultFileProvider = new DefaultFileProvider(Path.Combine(Application.dataPath, "Mods"));
 
-        public EditorCollectionProvider() : base(Version.GetDefaultFullVersionFormat())
-        {
+		public EditorCollectionProvider() : base(ModingVersion.GetDefaultFullVersionFormat())
+		{
 
-        }
+		}
 
-        protected override VersionProvider[] providers { get; set; } =
-        {
-            new(Version.GetFullVersionFormat(),
-                new ModProvider(typeof(ModMeta), new MetaProvider<ModMeta>(DefaultFileProvider, string.Empty)),
-                new ModProvider(typeof(StaticHierarchyMeta), new HierarchiesMetaProvider<StaticHierarchyMeta>(DefaultFileProvider)),
-                new ModProvider(typeof(PrefabHierarchyMeta), new PrefabsMetaProvider<PrefabHierarchyMeta>(DefaultFileProvider)),
-                new ModProvider(typeof(UnityPrefabInstance), new ObjMtlExporterProvider(DefaultFileProvider)),
-                new ModProvider(typeof(Texture2D), new TexturePngProvider(DefaultFileProvider)),
-                new ModProvider(typeof(LodHierarchyMeta), new LodInstanceProvider<LodHierarchyMeta>(DefaultFileProvider)),
-                new ModProvider(typeof(GameMarkerMeta), new GameMarkerInstanceProvider<GameMarkerMeta>(DefaultFileProvider))
-            ),
-        };
-    }
+		protected override VersionProvider[] providers { get; set; } =
+		{
+			new(ModingVersion.GetFullVersionFormat(),
+				new ModProvider(typeof(ModMeta), new MetaProvider<ModMeta>(DefaultFileProvider, string.Empty)),
+				new ModProvider(typeof(StaticHierarchyMeta), new HierarchiesMetaProvider<StaticHierarchyMeta>(DefaultFileProvider)),
+				new ModProvider(typeof(PrefabHierarchyMeta), new PrefabsMetaProvider<PrefabHierarchyMeta>(DefaultFileProvider)),
+				new ModProvider(typeof(UnityPrefabInstance), new ObjMtlExporterProvider(DefaultFileProvider)),
+				new ModProvider(typeof(Texture2D), new TexturePngProvider(DefaultFileProvider)),
+				new ModProvider(typeof(LodHierarchyMeta), new LodInstanceProvider<LodHierarchyMeta>(DefaultFileProvider)),
+				new ModProvider(typeof(GameMarkerMeta), new GameMarkerInstanceProvider<GameMarkerMeta>(DefaultFileProvider))
+			),
+		};
+	}
 }
--- Runtime/ModingVersion.cs
+++ Runtime/ModingVersion.cs
@@ -1,10 +1,10 @@
 namespace Plugins.CarX.Modding.Creator.Runtime
 {
-	public static class Version
+	public static class ModingVersion
 	{
-		private const string Uploader = "2.1";
-		private const string FormatVersion = "2026.3";
-		private const string DefaultFormatVersion = "2026.3";
+		private const string Uploader = "3.0";
+		private const string FormatVersion = "1.0";
+		private const string DefaultFormatVersion = "1.0";
 
 		public static string GetFullVersion() => $"v{Uploader}";
 		public static string GetFullVersionFormat() => $"v{FormatVersion}";
