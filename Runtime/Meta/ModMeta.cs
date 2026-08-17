52cba772052a54fba9d71d0a48c897b8f6dc3114
Evgenie Myasnikov <e.myasnikov@carx-tech.com>
2026-08-17 09:20:02 +0300

    map

--- Runtime/Meta/ModMeta.cs
+++ Runtime/Meta/ModMeta.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 namespace Plugins.CarX.Modding.Creator.Runtime
 {
 	[Serializable]
+	public class ModMinimapMeta
+	{
+		public string[] textures;
+		public float boundsCenterX;
+		public float boundsCenterY;
+		public float boundsSizeX;
+		public float boundsSizeY;
+	}
+
+	[Serializable]
 	public class ModMeta : IModResources, IModResourcesVersion
 	{
 		public string Id
@@ -24,6 +34,7 @@ namespace Plugins.CarX.Modding.Creator.Runtime
 		public string description;
 		public string icon;
 		public string largeIcon;
+		public ModMinimapMeta minimap;
 		public string madeIn;
 		public string url;
 		public string[] authors;
